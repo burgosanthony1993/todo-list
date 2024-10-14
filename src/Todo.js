b69a098ecea651eaa1fb95997afd805c45575970
Anthony Burgos <burgosanthony1993@gmail.com>
2024-10-13 20:40:08 -0400

    you can now edit the topic thingToDo. Next update to introduce edit dueDate and priority

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,13 @@
 //Child Componenet
 //Display a div with the task of the todo
 
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 class Todo extends Component {
+    state = {
+        isEditing: false,
+        thingsToDo: this.props.thingsToDo
+    };
 
     handleRemove = (evt) => {
         evt.preventDefault();
@@ -11,18 +15,52 @@ class Todo extends Component {
         this.props.removeItem();
     };
 
-    render(){
-        //Access to the props passed down from the parent
-        //returns display props
-        const {thingsToDo, dueDate,priority} = this.props;
-        return (
-            <div>
-                <h3>Task: {thingsToDo}</h3>
-                <p>Due Date: {dueDate}</p>
-                <p>Priority: {priority}</p>
-                <button onClick={this.handleRemove}>X to Delete</button>
-            </div>
-        )
+    toggleForm = () => {
+        this.setState({ isEditing: !this.state.isEditing });
+    };
+
+    handleUpdate = (evt) => {
+        evt.preventDefault();
+        // Take new task data and pass up to parent
+        this.props.updatedTodos(this.props.id, this.state.thingsToDo);
+        this.setState({ isEditing: false });
+    };
+
+    handleChange = (evt) => {
+        const { value } = evt.target;
+        this.setState({ thingsToDo: value });
+    };
+
+    render() {
+        let result;
+        if (this.state.isEditing) {
+            result = (
+                <div>
+                    <form onSubmit={this.handleUpdate}>
+                        <input
+                            type="text"
+                            value={this.state.thingsToDo}
+                            name="thingsToDo"
+                            onChange={this.handleChange}
+                        />
+                        <button>Save</button>
+                    </form>
+                </div>
+            );
+        } else {
+            const { thingsToDo, dueDate, priority } = this.props;
+            result = (
+                <div>
+                    <h3>Task: {thingsToDo}</h3>
+                    <p>Due Date: {dueDate}</p>
+                    <p>Priority: {priority}</p>
+                    <button onClick={this.toggleForm}>Edit</button>
+                    <button onClick={this.handleRemove}>X to Delete</button>
+                </div>
+            );
+        }
+
+        return result;
     }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,35 +3,30 @@
 should render the list of Todo components. 
 Please the states that contains all of the todos in this component*/
 
-import React, {Component} from "react"
-import { v4 as uuidv4 } from 'uuid';
-import Todo from "./Todo"
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
 import NewTodoForm from "./NewTodoForm";
 
 class TodoList extends Component {
-    //Public Class Field Syntax Method of declaring props
     state = {
-        //items list is an array of examples
         items: [
-            {id: uuidv4(), thingsToDo: "Complete Project", dueDate: "Next Week", priority: "Low"},
-            {id: uuidv4(), thingsToDo: "Laundry", dueDate: "Tomorrow", priority: "High"},
-            {id: uuidv4(), thingsToDo: "Submit Form", dueDate: "Today at 4 pm", priority: "Very High"}
+            { id: uuidv4(), thingsToDo: "Complete Project", dueDate: "Next Week", priority: "Low" },
+            { id: uuidv4(), thingsToDo: "Laundry", dueDate: "Tomorrow", priority: "High" },
+            { id: uuidv4(), thingsToDo: "Submit Form", dueDate: "Today at 4 pm", priority: "Very High" }
         ],
-        //props to be passed down to child
         newItem: {
             thingsToDo: "",
             dueDate: "",
             priority: ""
         }
-    }
+    };
 
-    //Using PCFS, Arrow Functions automatically bind this
     handleSubmit = (evt) => {
         evt.preventDefault();
-        // Update items with the new item details
         this.setState((prevState) => ({
             items: [...prevState.items, { ...prevState.newItem, id: uuidv4() }],
-            newItem: { thingsToDo: '', dueDate: '', priority: '' } // Resetting newItem state
+            newItem: { thingsToDo: "", dueDate: "", priority: "" }
         }));
     };
 
@@ -47,37 +42,46 @@ class TodoList extends Component {
 
     removeItem = (id) => {
         this.setState((st) => ({
-        items: st.items.filter((item) => item.id !== id),
+            items: st.items.filter((item) => item.id !== id)
         }));
     };
 
-    //maps array renders a list (iterate through a list of anything)
-    render(){
+    update = (id, updatedTask) => {
+        const updatedTodos = this.state.items.map((todo) => {
+            if (todo.id === id) {
+                return { ...todo, thingsToDo: updatedTask };
+            }
+            return todo;
+        });
+        this.setState({ items: updatedTodos });
+    };
+
+    render() {
         return (
             <div>
                 <h1>Todo List</h1>
                 <ul>
-                {this.state.items.map(item => (
+                    {this.state.items.map((item) => (
                         <li key={item.id}>
-                            {/* Pass props to Todo component */}    
-                            <Todo 
-                            key={item.id}
-                            id={item.id}
-                            thingsToDo={item.thingsToDo}
-                            dueDate={item.dueDate}
-                            priority={item.priority}
-                            removeItem={() => this.removeItem(item.id)}
+                            <Todo
+                                key={item.id}
+                                id={item.id}
+                                thingsToDo={item.thingsToDo}
+                                dueDate={item.dueDate}
+                                priority={item.priority}
+                                removeItem={() => this.removeItem(item.id)}
+                                updatedTodos={this.update}
                             />
                         </li>
-                ))}
+                    ))}
                 </ul>
-                    <NewTodoForm
-                        newItem={this.state.newItem}
-                        handleSubmit={this.handleSubmit}
-                        handleChange={this.handleChange}
-                    />
+                <NewTodoForm
+                    newItem={this.state.newItem}
+                    handleSubmit={this.handleSubmit}
+                    handleChange={this.handleChange}
+                />
             </div>
-        )
+        );
     }
 }
 
